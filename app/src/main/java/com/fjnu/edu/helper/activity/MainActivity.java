d5bb819e0dd437beafcad4c3a485bddc051302e5
lxf <869907853@qq.com>
2017-06-02 15:48:49 +0800

    update

--- app/src/main/java/com/fjnu/edu/helper/activity/MainActivity.java
+++ app/src/main/java/com/fjnu/edu/helper/activity/MainActivity.java
@@ -4,10 +4,15 @@ import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.fjnu.edu.helper.R;
 import com.fjnu.edu.helper.fragment.FoodMainFragment;
+import com.fjnu.edu.helper.view.SlidingMenu;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,6 +22,8 @@ public class MainActivity extends AppCompatActivity {
     private Button person;
 
     private FoodMainFragment foodmain;
+
+    private SlidingMenu menu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,9 +35,12 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void findview() {
+        menu=(SlidingMenu)findViewById(R.id.id_menu);
+
     }
 
     private void initview() {
+
     }
 
     private void setDefaultFragment() {
@@ -39,5 +49,10 @@ public class MainActivity extends AppCompatActivity {
         foodmain = new FoodMainFragment();
         transaction.replace(R.id.main_content, foodmain);
         transaction.commit();
+    }
+
+    public void toggleMenu(View view)
+    {
+        menu.toggle();
     }
 }
