cfe718c74f3d4ed7c530d157a80c84d7898a5fe2
lxf <869907853@qq.com>
2017-06-07 21:30:47 +0800

    update

---
+++ app/src/main/java/com/fjnu/edu/helper/activity/AddFoodActivity.java
@@ -0,0 +1,15 @@
+package com.fjnu.edu.helper.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.fjnu.edu.helper.R;
+
+public class AddFoodActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_food);
+    }
+}
